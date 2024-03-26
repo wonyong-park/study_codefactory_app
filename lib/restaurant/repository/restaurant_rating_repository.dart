a4d813f6965a109cb78a3950a03b4f116acc0116
wonyong <wanyong0919@naver.com>
2024-03-26 21:10:24 +0900

    RestaurantRatingRepository 작업

---
+++ lib/restaurant/repository/restaurant_rating_repository.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:study_codefactory_app/common/const/data.dart';
+import 'package:study_codefactory_app/common/dio/dio.dart';
+import 'package:study_codefactory_app/common/model/cursor_pagination_model.dart';
+import 'package:study_codefactory_app/common/model/pagination_params.dart';
+import 'package:study_codefactory_app/rating/model/rating_model.dart';
+
+part 'restaurant_rating_repository.g.dart';
+
+// http://$ip/restaurant/:rid/rating
+@RestApi()
+abstract class RestaurantRatingRepository {
+  factory RestaurantRatingRepository(Dio dio, {String baseUrl}) = _RestaurantRatingRepository;
+
+  @GET('/')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<CursorPagination<RatingModel>> paginate({
+    @Queries() PaginationParams? paginationParams = const PaginationParams(),
+  });
+
+}
