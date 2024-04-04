2a628734f3f03a7bcb78606745103c84159f5517
wonyong <wanyong0919@naver.com>
2024-04-04 22:50:59 +0900

    OrderPagination 작업

--- lib/order/component/order_card.dart
+++ lib/order/component/order_card.dart
@@ -40,18 +40,21 @@ class OrderCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Text(
           '${orderDate.year}.${orderDate.month.toString().padLeft(2, '0')}.${orderDate.day.toString().padLeft(2, '0')} 주문 완료',
         ),
+        const SizedBox(height: 8.0),
         Row(
           children: [
             ClipRRect(
               borderRadius: BorderRadius.circular(16.0),
               child: image,
             ),
-            const SizedBox(height: 16.0),
+            const SizedBox(width: 16.0),
             Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   name,
--- lib/order/view/order_screen.dart
+++ lib/order/view/order_screen.dart
@@ -1,19 +1,21 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:flutter/material.dart';
-import 'package:study_codefactory_app/common/layout/default_layout.dart';
+import 'package:study_codefactory_app/common/component/pagination_list_view.dart';
+import 'package:study_codefactory_app/order/component/order_card.dart';
+import 'package:study_codefactory_app/order/model/order_model.dart';
+import 'package:study_codefactory_app/order/provider/order_provider.dart';
 
 class OrderScreen extends ConsumerWidget {
   const OrderScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return DefaultLayout(
-      child: Center(
-        child: Container(
-          child: Text('주문'),
-        ),
-      ),
+    return PaginationListView<OrderModel>(
+      provider: orderProvider,
+      itemBuilder: <OrderModel> (_, index, model) {
+        return OrderCard.fromModel(model: model);
+      },
     );
   }
 }
